7b1a8625e709abdf5f122124748629db1c680c7f
Lakshya Sharma <lakahyasharma26@gmail.com>
2026-04-25 15:49:38 +0530

    Update authmiddleware.js

--- authmiddleware.js
+++ authmiddleware.js
@@ -7,12 +7,11 @@ function isAuthenticated(req, res, next) {
 }
 
 // 🔥 RBAC
-function isAdmin(req, res, next) {
-  if (req.session.user && req.session.user.role === "admin") {
-    next();
-  } else {
-    res.send("Access Denied ❌ (Admin only)");
-  }
-}
 
 module.exports = { isAuthenticated, isAdmin };
+function isAdmin(req, res, next) {
+  if (req.session.user && req.session.user.role === "admin") {
+    return next();
+  }
+  res.send("Access Denied ❌ (Admin only)");
+}
